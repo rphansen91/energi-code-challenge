c04c4522153be6749f70417e0a69ddcfffbfebcf
rphansen91 <rphansen91@gmail.com>
2020-09-03 07:02:27 -0700

    Rename filter fns to isFirstDupe and isLastDupe

--- lib/deduper.ts
+++ lib/deduper.ts
@@ -1,11 +1,11 @@
 type Maybe<V> = V | null | undefined
 type IsTruthyFn<V> = (v: V, i: number, vs: V[]) => Maybe<boolean>
 
-export const isDupe: IsTruthyFn<number> = (num, i, nums) => nums.indexOf(num) === i
-export const deduper = filterFp(isDupe)
+export const isFirstDupe: IsTruthyFn<number> = (num, i, nums) => nums.indexOf(num) === i
+export const deduper = filterFp(isFirstDupe)
 
-export const isDupeAlt: IsTruthyFn<number> = (num, i, nums) => nums.lastIndexOf(num) === i
-export const deduperAlt = filterFp(isDupeAlt)
+export const isLastDupe: IsTruthyFn<number> = (num, i, nums) => nums.lastIndexOf(num) === i
+export const deduperAlt = filterFp(isLastDupe)
 
 // Functional filter
 function filterFp<V>(fn: IsTruthyFn<V>) {
